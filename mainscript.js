78f6c5443e86052d371a0086ae47c1a1864b658d
Phuc <thegifted165@gmail.com>
2024-04-19 21:31:49 +0700

    UPDATE

---
+++ mainscript.js
@@ -0,0 +1,19 @@
+let text=document.getElementById('text');
+let sun=document.getElementById('sun');
+let cloud1=document.getElementById('cloud1');
+let cloud2=document.getElementById('cloud2');
+let tree=document.getElementById('tree');
+let hill1=document.getElementById('hill1');
+let hill2=document.getElementById('hill2');
+let hill3=document.getElementById('hill3');
+window.addEventListener('scroll', () =>
+{
+    let value = window.scrollY;
+    text.style.marginTop = value * 2 + 'px';
+    sun.style.top = value * -1.5 + 'px';
+    hill2.style.left = value * 1.5 + 'px';
+    hill1.style.left = value * -1.5 + 'px'
+    tree.style.top = value * 0.5 + 'px';
+    cloud1.style.left = value * -2 + 'px';
+    cloud2.style.left = value * 2 + 'px';
+});
